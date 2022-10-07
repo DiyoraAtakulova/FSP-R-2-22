352194df3fda731e4b2b3d30af3443d175291c54
DiyoraAtakulova <atakulovadiyora@gmail.com>
2022-10-07 15:12:41 -0400

    Функциональное пограммирование

--- semester_2/lesson_14/lesson_14.py
+++ semester_2/lesson_14/lesson_14.py
@@ -17,14 +17,34 @@
 Функциональное пограммирование
 
 """
-def person():
-    pass
+# def person(a,b,  c):
+#     pass
 
-p = person()
-p_2 = person()
+# p = person(1, 2, 3)
+# p_2 = person()
 
+
+# dunder/magic functions __init__
+# Названия в классах каждая первая буква с большой буквы
 class Person:
-    pass
+    def __init__(self, name, surname, age): # self это ссылка к методу классу
+        self.name = name
+        self.surname = surname
+        self.age = age
+    
+    def get_full_name(self):
+        return f"My name is {self.name} and my surname {self.surname}"
+
+
 # инициализировать класс - создать/вызвать
 # и создать экземпляр класса
-person = Person()
+person_1 = Person("Aybek", "Tordaliev", 15)
+print(person_1.name, person_1.surname, person_1.age)
+full_name = person_1.get_full_name()
+print(full_name)
+
+person_2 = Person("Kim", "Pak", 45)
+print(person_2.name, person_2.surname, person_2.age)
+print(person_2.get_full_name())
+
+# print(person_1, person_2, dir(Person))
