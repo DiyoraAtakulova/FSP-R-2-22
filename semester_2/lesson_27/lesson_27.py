7bc7d7727e69a2a2b51b932b6611ef0da14ff920
DiyoraAtakulova <atakulovadiyora@gmail.com>
2022-11-24 13:01:49 -0500

    lesson_27

---
+++ semester_2/lesson_27/lesson_27.py
@@ -0,0 +1,28 @@
+"""
+
+(pip install --upgrade pip)
+
+pip install -r path_to_the_with_packages:A
+
+git status ->> git add ->> git commit ->>
+
+git branch  
+Создаете новую ветку, переходите туда 
+git checkout -b имя_ветки 
+ 
+Делаете изменения 
+Фиксируете изминения 
+Опубликовываете их 
+Создаете Pull request 
+ 
+ 
+Когда вы в первый раз публикуете изменения из новой ветки в гитхаб, 
+вам нужно связать новую ветку 
+с глобальной веткой и создать глобальную ветку. 
+Для этого есть две команды 
+1. git push -u origin название_ветки 
+2. git push --set-upstream origin название_ветки 
+ 
+git stash  
+git pull
+"""
