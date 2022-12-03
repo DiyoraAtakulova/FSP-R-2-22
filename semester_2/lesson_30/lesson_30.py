768fb7a418766368fbe8bc6d6511e780632b4be6
DiyoraAtakulova <atakulovadiyora@gmail.com>
2022-12-03 03:10:33 -0500

    Translator

---
+++ semester_2/lesson_30/lesson_30.py
@@ -0,0 +1,14 @@
+from translate import Translator
+
+trn_ru_to_en = Translator(from_lang='ru', to_lang='en')
+text = ''
+
+with open('russian.txt', mode='r+', encoding='utf-8') as file:
+    for i in file:
+        text += i
+
+with open("english.txt", mode='w') as file:
+    total = trn_ru_to_en.translate(text)
+    file.write(total)
+
+print(total)
